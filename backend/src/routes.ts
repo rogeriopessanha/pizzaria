fccb1fe073a0f7013639f7d2bea068a798b7651a
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-26 16:54:08 -0300

    att

---
+++ backend/src/routes.ts
@@ -0,0 +1,13 @@
+
+
+import {Router, Request, Response} from 'express'
+
+const router = Router()
+
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({nome: "Pizzaria"})
+    // throw new Error("Erro ao fazer essa requisão");
+    
+})
+
+export {router}
