72573a6f7eddace77b4a296f9847bdc48d4a660f
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-27 02:24:16 -0300

    att

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 
 
 import {Router} from 'express'
+
 import {CreateUserController} from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import {DetailUserController} from './controllers/user/DetailUserController'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router()
@@ -14,5 +18,9 @@ router.post('/users', new CreateUserController ().handle)
 router.post('/session', new AuthUserController ().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController ().handle)
+
+
+//-- ROTAS CATEGORY --
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export {router}
