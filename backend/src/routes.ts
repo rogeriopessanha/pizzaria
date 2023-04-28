7ca4206d1f6ee9e0b2af7fe40ea629d390cc77ea
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-28 01:53:41 -0300

    att

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -22,6 +22,8 @@ import { RemoveOrderController } from './controllers/order/RemoveOrderController
 import { AddItemController } from './controllers/order/AddItemController'
 import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/sendOrderController'
+import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrderController } from './controllers/order/DetailOrderController'
 
 //middlewares de autenticação
 import { isAuthenticated } from './middlewares/isAuthenticated'
@@ -31,7 +33,6 @@ import uploadConfig from './config/multer'
 
 
 const router = Router()
-
 const upload = multer(uploadConfig.upload("./temp"))
 
 //-- ROTAS USER --
@@ -63,5 +64,9 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+
+router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
 
 export {router}
