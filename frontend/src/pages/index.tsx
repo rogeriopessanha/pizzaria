f97eccbd9b40f438abb9e8b902143ef12bb8367d
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-29 01:00:48 -0300

    att

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,49 @@
 
+import Head from "next/head"
+import Image from "next/image"
+import styles from '../styles/home.module.scss'
+
+import logoImg from '../../public/assets/logo2.svg'
+
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
 
 export default function Home() {
   return (
-<div>
-  <h1>Olá mundo!</h1>
-</div>
+    <>
+      <Head>
+        <title>Pizzaria - Faça seu login</title>
+      </Head>
+
+      <div className={styles.containerCenter}>
+        <Image className={styles.image} src={logoImg} alt="Logo Pizzaria" />
+
+        <div className={styles.login}>
+          <form action="">
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+            />
+
+            <Input
+              placeholder="Digite sua senha"
+              type="password"
+            />
+
+            <Button
+            type="submit"
+            loading={false}
+            >
+              Acessar
+            </Button>
+
+
+          </form>
+
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+
+        </div>
+      </div>
+    </>
   )
 }
