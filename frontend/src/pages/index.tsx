134b00b33741e33683b4be93cb34e4e5acb21759
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-05-01 23:12:29 -0300

    att

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,6 @@
 
+import {useContext, FormEvent, useState, ChangeEvent} from 'react'
+
 import Head from "next/head"
 import Image from "next/image"
 import styles from '../styles/home.module.scss'
@@ -8,7 +10,28 @@ import logoImg from '../../public/assets/logo2.svg'
 import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
 
+import { AuthContext } from "@/contexts/AuthContext"
+
+import Link from "next/link"
+
 export default function Home() {
+  const {signIn} = useContext(AuthContext)
+
+  const [email, SetEmail] = useState('')
+  const [password, SetPassword] = useState('')
+
+  const [loading, SetLoading] = useState(false)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault()
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
   return (
     <>
       <Head>
@@ -19,28 +42,39 @@ export default function Home() {
         <Image className={styles.image} src={logoImg} alt="Logo Pizzaria" />
 
         <div className={styles.login}>
-          <form action="">
+
+        <h1>Entre na sua conta</h1>
+
+          <form action="" onSubmit={handleLogin}>
+
             <Input
               placeholder="Digite seu email"
               type="text"
+              value={email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => SetEmail(e.target.value)}
             />
 
             <Input
               placeholder="Digite sua senha"
               type="password"
+              value={password}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => SetPassword(e.target.value)}
             />
 
             <Button
             type="submit"
             loading={false}
             >
-              Acessar
+              Entrar
             </Button>
 
 
           </form>
 
+          <Link href="/signup" legacyBehavior>
           <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          </Link>
+
 
         </div>
       </div>
