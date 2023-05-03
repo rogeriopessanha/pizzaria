7ebe78b021baa5954da35aa139f6cd6eb1573267
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-05-03 14:08:14 -0300

    att

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
 
+import { useContext, FormEvent } from 'react'
 import Head from "next/head"
 import Image from "next/image"
 import styles from '@/styles/home.module.scss'
@@ -9,9 +10,25 @@ import logoImg from '../../public/logo.svg'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
+import {AuthContext} from '@/contexts/AuthContext'
+
 import Link from "next/link"
 
 export default function Home() {
+
+  const {signIn} = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    let data = {
+      email: "alguma@teste.com",
+      password: "231324654"
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
       <Head>
@@ -23,7 +40,7 @@ export default function Home() {
         <div className={styles.login}>
         <h1>Entre na sua conta</h1>
         
-          <form>
+          <form onSubmit={handleLogin}>
             <Input
             placeholder="Digite seu email"
             type="text"
